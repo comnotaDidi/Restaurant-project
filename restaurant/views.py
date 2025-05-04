af17bc542798457ba6978a9a1b64f018c63cd217
anton <anton@gmail.com>
2025-05-04 14:16:25 +0200

    New Update

--- restaurant/views.py
+++ restaurant/views.py
@@ -13,21 +13,56 @@ class HomeView(TemplateView):
     template_name = 'restaurant/home.html'
 
 # Menu Page
+from django.views.generic import TemplateView
+
 class MenuView(TemplateView):
     template_name = 'restaurant/menu.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
         context['food'] = [
-            # same food list as before...
+            {"name": "Kawior Antonius", "description": "kawior Antonius, crème fraîche, bliny, złoto", "price": "350 PLN"},
+            {"name": "Sushi Bites", "description": "ryż do sushi, tuńczyk błękitnopłetwy, łosoś, seriola, dodatki", "price": "72 PLN"},
+            {"name": "Tartaletka z tuńczykiem", "description": "tuńczyk błękitnopłetwy, emulsja nori, kawior", "price": "88 PLN"},
+            {"name": "Ostryga Gillardeau / 1 szt.", "description": "ostryga, imbir, zielone jabłko, kawior limonkowy", "price": "42 PLN"},
+            {"name": "Bubu arare", "description": "brokuły, krem z awokado, perły ryżowe", "price": "48 PLN"},
+            {"name": "Tsukemono", "description": "ogórek, sezam", "price": "18 PLN"},
+            {"name": "Tatar wołowy", "description": "wołowina black angus, domowy tost, kapary, marynowane szalotki", "price": "84 PLN"},
+            {"name": "Tuna tataki", "description": "tuńczyk błękitnopłetwy, awokado, ogórek, imbir, yuzu", "price": "92 PLN"},
+            {"name": "Tuna toro & crispy rice", "description": "tuńczyk błękitnopłetwy, ryż, aioli z pora", "price": "82 PLN"},
+            {"name": "Ravioli z łososiem", "description": "łosoś, seriola, kalarepa, pomidor dashi", "price": "98 PLN"},
+            {"name": "Sałatka z okoniem", "description": "chrupiący okoń morski, pomelo, karmelizowane orzechy nerkowca, zioła", "price": "88 PLN"},
+            {"name": "Shrimp har gao", "description": "pierożki z krewetkami, bisque z kraba, słodki olej chilli", "price": "72 PLN"},
+            {"name": "Nasu miso", "description": "bakłażan, słodka glazura miso, sezam, szczypiorek", "price": "48 PLN"},
+            {"name": "Ebi tempura", "description": "krewetki w tempurze, majonez japoński", "price": "92 PLN"},
+            {"name": "Gyoza", "description": "wieprzowina lub kurczak, zielona cebulka, sos orientalny", "price": "68 PLN"},
+            {"name": "Buns z szarpaną wieprzowiną", "description": "szarpana wieprzowina, orientalne warzywa, Sechuan, Sriracha", "price": "72 PLN"},
+            {"name": "Polędwica wołowa Black Angus (170g)", "description": "pak choi, purée ziemniaczane z wasabi, sezam, sos orientalny", "price": "158 PLN"},
         ]
+
         context['desserts'] = [
-            # same desserts list as before...
+            {"name": "Choux", "description": "biała czekolada, owoce jagodowe, matcha anglaise", "price": "42 PLN"},
+            {"name": "Pomelo", "description": "mus z białej czekoladowy, wanilia, biszkopt kokosowy", "price": "42 PLN"},
+            {"name": "Torcik Ruby", "description": "czekolada Ruby, jeżyny, czerwone pomarańcze", "price": "65 PLN"},
+            {"name": "Lody", "description": "frosty & fruity", "price": "58 PLN"},
         ]
+
         context['cocktails'] = [
-            # same cocktails list as before...
+            {"name": "Above The Clouds", "description": "rum, olej kokosowy, masło kakaowe, likier słony karmel, dym, podawany z jadalnym złotem", "price": "—"},
+            {"name": "Royal", "description": "Szampan Moet & Chandon Brut Imperial", "price": "—"},
+            {"name": "Ichie", "description": "whiskey, ananas, marakuja, likier bananowy", "price": "—"},
+            {"name": "The Duchess", "description": "wódka, cytryna, trawa cytrynowa, maliny, imbir, lemoniada", "price": "—"},
+            {"name": "Bergamo", "description": "gin z kwiatem bzu, wino, różowy pieprz, zapach bergamotki, oliwa grejpfrutowa", "price": "—"},
+            {"name": "Botanist", "description": "gin, likier z kwiatu bzu, ogórek, cytryna, lemoniada różana", "price": "—"},
+            {"name": "Capuccini", "description": "bourbon, likier, mleko z orzechów laskowych, espresso, syrop klonowy, kropla śmietanki", "price": "—"},
+            {"name": "Ruby Sky", "description": "bourbon, martini, likier z orzechów laskowych, ocet wiśniowy, bitters czekoladowy", "price": "—"},
+            {"name": "Pink Flamingo", "description": "tequila, aperol, kordiał grejpfrutowy, nasiona kolendry, różowy pieprz, limonka, sól morska, woda gazowana", "price": "—"},
+            {"name": "Bison in the Moss", "description": "wódka żubrówka, jabłko, mięta, imbir, cytryna, rumianek", "price": "—"},
         ]
+
         return context
+
 
 # Gallery Page
 class GalleryView(TemplateView):
@@ -68,10 +103,7 @@ class ReviewThanksView(TemplateView):
 # Custom Login View
 class CustomLoginView(LoginView):
     template_name = 'restaurant/login.html'
-    authentication_form = LoginForm
-
-    def get_success_url(self):
-        return reverse_lazy('home')
+    authentication_form = AuthenticationForm
 
 # Custom Logout View
 class CustomLogoutView(LogoutView):
